6703e85766829a3861a3f4f5c03ed7a304fb9957
Mosaab.Ahmed <mosaab.ahmed@tooli.tv>
2020-10-25 17:53:59 +0200

    ui custom control

--- app/src/main/java/com/movie/exoplayerplayground/MindOrksActivity.kt
+++ app/src/main/java/com/movie/exoplayerplayground/MindOrksActivity.kt
@@ -1,9 +1,15 @@
 package com.movie.exoplayerplayground
 
+import android.content.pm.ActivityInfo
 import android.net.Uri
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
+import android.view.WindowManager
+import android.widget.PopupMenu
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.google.android.exoplayer2.ExoPlaybackException
 import com.google.android.exoplayer2.ExoPlayerFactory
 import com.google.android.exoplayer2.Player
@@ -14,15 +20,19 @@ import com.google.android.exoplayer2.source.dash.DashMediaSource
 import com.google.android.exoplayer2.upstream.DataSource
 import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
 import kotlinx.android.synthetic.main.activity_mind_orks.*
+import kotlinx.android.synthetic.main.exo_playback_control_view.view.*
+
 
 class MindOrksActivity : AppCompatActivity(), Player.EventListener {
 
     private lateinit var simpleExoplayer: SimpleExoPlayer
     private var playbackPosition: Long = 0
+    private val hls = "https://devstreaming-cdn.apple.com/videos/streaming/examples/bipbop_16x9/bipbop_16x9_variant.m3u8"
     private val mp4Url = "https://html5demos.com/assets/dizzy.mp4"
     private val dashUrl = "https://storage.googleapis.com/wvmedia/clear/vp9/tears/tears_uhd.mpd"
-    private val urlList = listOf(mp4Url to "default", dashUrl to "dash")
+    private val urlList = listOf(dashUrl to "dash")
 
+    var flag = false
     private val dataSourceFactory: DataSource.Factory by lazy {
         DefaultDataSourceFactory(this, "exoplayer-sample")
     }
@@ -30,7 +40,40 @@ class MindOrksActivity : AppCompatActivity(), Player.EventListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_mind_orks)
+
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        exoplayerView.bt_fullscreen.setOnClickListener{
+            if(flag){
+
+                exoplayerView.bt_fullscreen.setImageResource(R.drawable.ic_baseline_fullscreen_24)
+
+                requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT;
+                flag= false;
+            }else {
+                exoplayerView.bt_fullscreen.setImageResource(R.drawable.ic_baseline_fullscreen_exit_24)
+                requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE;
+                flag= true;
+            }
+        }
+
+        exoplayerView.bt_quality.setOnClickListener {
+            val popup = PopupMenu(this, it)
+            popup.setOnMenuItemClickListener(object : MenuItem.OnMenuItemClickListener,
+                PopupMenu.OnMenuItemClickListener {
+                override fun onMenuItemClick(menuItem: MenuItem?): Boolean {
+                    Toast.makeText(applicationContext, "Clicked", Toast.LENGTH_LONG).show()
+                    return false
+                }
+            })
+            val menu: Menu = popup.menu
+            menu.add(Menu.NONE, 0, 0, "Video quality")
+            popup.show()
+        }
+
+
     }
+
 
     override fun onStart() {
         super.onStart()
@@ -40,6 +83,7 @@ class MindOrksActivity : AppCompatActivity(), Player.EventListener {
     override fun onStop() {
         super.onStop()
         releasePlayer()
+        simpleExoplayer
     }
 
     private fun initializePlayer() {
@@ -52,6 +96,13 @@ class MindOrksActivity : AppCompatActivity(), Player.EventListener {
         simpleExoplayer.addListener(this)
     }
 
+    private fun preparePlayer(videoUrl: String, type: String) {
+        val uri = Uri.parse(videoUrl)
+        val mediaSource = buildMediaSource(uri, type)
+        simpleExoplayer.prepare(mediaSource)
+    }
+
+
     private fun buildMediaSource(uri: Uri, type: String): MediaSource {
         return if (type == "dash") {
             DashMediaSource.Factory(dataSourceFactory)
@@ -62,11 +113,6 @@ class MindOrksActivity : AppCompatActivity(), Player.EventListener {
         }
     }
 
-    private fun preparePlayer(videoUrl: String, type: String) {
-        val uri = Uri.parse(videoUrl)
-        val mediaSource = buildMediaSource(uri, type)
-        simpleExoplayer.prepare(mediaSource)
-    }
 
     private fun releasePlayer() {
         playbackPosition = simpleExoplayer.currentPosition
